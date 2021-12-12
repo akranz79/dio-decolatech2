2c5cff12ba3a3a4fd73078dad808d9fe51b588f3
akranz79 <ahkanz79@gmail.com>
2021-12-12 12:13:26 -0300

    Programando com Orientação a Objetos

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ExemploPOO.Helper;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+
+    {
+        static void Main(String[] args)
+        {
+            var caminho = "C:\\TrabalhandoComArquivos";
+
+            FileHelper helper = new FileHelper();
+            helper.ListarDiretorios(caminho);
+
+
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(10, 30);
+            // System.Console.WriteLine($"Area: {r.ObterArea()}");
+
+
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Alexandre";
+            // p1.Idade = 42;
+
+            // p1.Apresentar();
+        }
+    }
+}
